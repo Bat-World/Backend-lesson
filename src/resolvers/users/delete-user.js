3f55c01ad4ae943c03e68161c02d558d8eef3dfd
Second <drewuncle863724lu@gmail.com>
2025-02-23 11:40:53 +0800

    deleteuserfromJson

--- src/resolvers/users/delete-user.js
+++ src/resolvers/users/delete-user.js
@@ -1,10 +1,12 @@
-import { users } from "../../index.js";
-import express from "express";
+
+import fs from "fs";
 
 export const deleteUser = (req, res) => {
+  const users = JSON.parse(fs.readFileSync("src/DB/users.json"));
   if (users.length > 0) {
     const removedUser = users.pop();
     res.json(removedUser);
+    fs.writeFileSync("src/DB/users.json", JSON.stringify(users));
   } else {
     res.json({ message: "No users to delete" });
   }
--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,7 +1,7 @@
-import express from "express";
+
 import { Router } from "express";
-import { Authorization } from "../middleWare/authorization.js";
 import { getUser } from "../resolvers/users/get-user.js";
+import { Authorization } from "../middleWare/authorization.js";
 import { createUser } from "../resolvers/users/create-user.js";
 import { deleteUser } from "../resolvers/users/delete-user.js";
 import { updateUser } from "../resolvers/users/update-user.js";
